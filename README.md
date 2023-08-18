2c8a4118f5945a73e10b703e747d392cc2b08f4a
elias-eriksson <eliaseriksson95@gmail.com>
2023-08-19 00:54:59 +0200

    async create / delete

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lite-star
 Playground for lite-star
+
+## TODO
+* implement postgres
+  * change uuid id to server default 
+  * setup database tests to be able to use postgres
+  * setup CI to be able to use postgres [more reading...](https://medium.com/qest/database-for-ci-cd-tests-quickly-and-inexpensively-96e3116ce72f)
+* database abstraction
+* lite-start hello world
+* storing passwords
+* authentication
+* issue JWTs
+* `GET /auth`
+* test `GET /auth`
+* email verification?
--- database/test_database.py
+++ database/test_database.py
@@ -1,14 +1,32 @@
 import pytest
 from . import create
 from . import delete
+from .session import Session
+from . import models
+from sqlalchemy import select
 
 
 @pytest.fixture
-def setup():
-    create()
+async def setup():
+    await create()
     yield None
-    delete()
+    await delete()
 
 
-async def test_user() -> None:
-    assert 1 + 1 == 2
+async def test_user(setup: None) -> None:
+    async with Session() as session:
+        users = await session.execute(select(models.User))
+        assert len(users.scalars().all()) == 0
+        await session.commit()
+
+        async with session.begin():
+            users = [
+                models.User(email="jessie@rocket.com"),
+                models.User(email="james@rocket.com"),
+                models.User(email="giovani@rocket.com"),
+            ]
+            session.add_all(users)
+
+        users = await session.execute(select(models.User))
+        assert len(users.scalars().all()) == 3
+        await session.commit()
