f18b2e8930c1ecb8244c1b784b5b4e2a6f09ca32
elias-eriksson <eliaseriksson95@gmail.com>
2023-08-27 02:09:28 +0200

    alembic CLI work

--- cli/database.py
+++ cli/database.py
@@ -10,13 +10,21 @@ from alembic import command
 cli = click.Group("database")
 
 
-def database_credentials(function):
+def database_credentials(command):
     options = (
         click.option(
-            "--username", "-u", "POSTGRES_USERNAME", type=str, help="Postgres username."
+            "--username",
+            "-u",
+            "POSTGRES_USERNAME",
+            type=str,
+            help="Postgres username.",
         ),
         click.option(
-            "--password", "-p", "POSTGRES_PASSWORD", type=str, help="Postgres password."
+            "--password",
+            "-p",
+            "POSTGRES_PASSWORD",
+            type=str,
+            help="Postgres password.",
         ),
         click.option(
             "--database",
@@ -40,29 +48,49 @@ def database_credentials(function):
             help="Port used by the Postgres database server.",
         ),
     )
-    return reduce(lambda result, option: option(result), options, function)
+    return reduce(lambda result, option: option(result), options, command)
 
 
 @cli.command()
 @database_credentials
-def create(**options: str | int) -> None:
-    configuration = DatabaseConfiguration(options)
+def create(**credentials: str | int) -> None:
+    configuration = DatabaseConfiguration(credentials)
     database = Database(configuration)
     asyncio.run(database.create())
 
 
 @cli.command()
 @database_credentials
-def delete(**options: str | int) -> None:
-    configuration = DatabaseConfiguration(options)
+def delete(**credentials: str | int) -> None:
+    configuration = DatabaseConfiguration(credentials)
     database = Database(configuration)
     asyncio.run(database.delete())
 
 
 @cli.command()
+@click.option("--message", "-m", type=str, help="Revision message", default=None)
 @database_credentials
-def revision(**options: str | int) -> None:
-    configuration = DatabaseConfiguration(options)
-    alembic_configuration = AlembicConfiguration()
-    command.revision()
-    # options = {f"POSTGRES_{name.upper()}": value for name, value in options.items()}
+def revision(message: str | None, **credentials: str | int) -> None:
+    DatabaseConfiguration(credentials)
+    command.revision(
+        AlembicConfiguration("./alembic.ini"),
+        autogenerate=True,
+        message=message,
+    )
+
+
+# merge these commands
+@cli.command()
+@click.argument("revision", type=str, help="Revision id.", default="head")
+@database_credentials
+def migrate(revision: str, **credentials: str | int) -> None:
+    DatabaseConfiguration(credentials)
+    command.upgrade(AlembicConfiguration("./alembic.ini"), revision)
+
+
+@cli.command()
+@click.argument("revision", type=str, help="alembic revision.", required=True)
+@database_credentials
+def downgrade(revision: str, **credentials: str | int) -> None:
+    DatabaseConfiguration(credentials)
+    command.downgrade(AlembicConfiguration("./alembic.ini"), revision)
