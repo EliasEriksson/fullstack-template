8557b872d5eb47dc70fcc1cc7e6988c05976584d
eliaseriksson <eliaseriksson95@gmail.com>
2023-09-03 09:32:07 +0200

    setup cli to launch api with uvicorn

--- cli/api.py
+++ cli/api.py
@@ -1,3 +1,5 @@
+from typing import Literal
+import uvicorn
 import click
 
 
@@ -5,6 +7,10 @@ cli = click.Group("api")
 
 
 @cli.command()
-@click.option("-p", "--port", default=8080)
-def start(port) -> None:
-    click.echo(f"api start {port}")
+@click.argument("mode", type=click.Choice(["prod", "dev"]), default="dev")
+@click.option("-p", "--port", type=int, default=8080)
+def start(mode: Literal["prod"] | Literal["dev"], port: int) -> None:
+    if mode == "prod":
+        uvicorn.run("api:app", port=port, log_level="info")
+    else:
+        uvicorn.run("api:app", reload=True, port=port, log_level="info")
