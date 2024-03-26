52d5ac8b2f751fc02193a81168b28d9ccfe7d333
Elias Eriksson <eliaseriksson95@gmail.com>
2024-03-26 22:19:31 +0100

    cli overrides env

--- tests/configuration/database/test_configuration.py
+++ tests/configuration/database/test_configuration.py
@@ -62,3 +62,33 @@ async def test_modified_defaults(environment: None) -> None:
     assert port == 2345
     url = configuration.database.url
     assert url == f"postgresql+psycopg://{username}:{password}@{host}:{port}/{name}"
+
+
+async def test_from_environment(environment: None) -> None:
+    os.environ.clear()
+    os.environ.update(
+        {
+            ConfigurationVariables.mode: "dev",
+            Variables.username: "fullstack-not",
+            Variables.password: "the-password",
+            Variables.host: "192.168.1.20",
+            Variables.port: "2345",
+            Variables.name: "the-database-name",
+        }
+    )
+    cli = {
+        Variables.port: 3245,
+    }
+    configuration = Configuration(cli=cli)
+    username = configuration.database.username
+    assert username == "fullstack-not"
+    password = configuration.database.password
+    assert password == "the-password"
+    name = configuration.database.name
+    assert name == "the-database-name"
+    host = configuration.database.host
+    assert host == "192.168.1.20"
+    port = configuration.database.port
+    assert port == 3245
+    url = configuration.database.url
+    assert url == f"postgresql+psycopg://{username}:{password}@{host}:{port}/{name}"
