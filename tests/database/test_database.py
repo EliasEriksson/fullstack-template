727008525514b816933751812779fde22e56a307
Elias Eriksson <eliaseriksson95@gmail.com>
2024-03-26 23:34:27 +0100

    ci debugging

--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -9,6 +9,7 @@ from shared import hash
 @pytest.fixture
 async def database():
     configuration = Configuration()
+    print("USING MODE:", configuration.mode)
     print("USING CONNECTION URL:", configuration.database.url)
     database = Database(configuration)
     await database.create()
