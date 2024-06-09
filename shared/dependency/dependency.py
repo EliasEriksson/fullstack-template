38f841bc9a02152f7c45ec8b44aa2ab09414a610
eliaseriksson <eliaseriksson95@gmail.com>
2024-06-09 02:10:23 +0200

    linting

--- shared/dependency/dependency.py
+++ shared/dependency/dependency.py
@@ -30,8 +30,7 @@ class Dependency(ABC):
 
     @classmethod
     @abstractmethod
-    def name(cls) -> str:
-        ...
+    def name(cls) -> str: ...
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}()"
